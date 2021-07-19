0fe2ff0ba0f659847f0253d76ca7c558b388a4d8
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2021-07-19 19:26:42 +0530

    Backend architecture design, backend express routes learned

---
+++ testback/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8000;
+
+app.get('/', (req, res) => {
+    return res.send('This is home page');
+})
+
+app.get('/login', (req, res) => {
+    return res.send('You are using Login route');
+})
+
+app.get('/signout', (req, res) => {
+    return res.send("You are signed out");
+})
+
+app.get('/atharva', (req, res) => {
+    return res.send('Atharva loves Twitter');
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
