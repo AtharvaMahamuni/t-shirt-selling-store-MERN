00583df8b8b3b9b82d29a8f03efe026e5aaa8b20
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2021-07-28 23:13:32 +0530

    user login with authentication

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-const { signout, signup } = require("../controllers/auth");
+const { signout, signup, signin } = require("../controllers/auth");
 // const { body, validationResult } = require('express-validator');
 const { check } = require('express-validator');
 
 
 router.post("/signup", [
-    check("name", "Name should be at least 3 char").isLength({ min:3 }),
-    check("email", "email is required").isEmail(),
-    check("password", "password is should be at least 3 char").isLength({ min: 3 })
-],signup);
+        check("name", "Name should be at least 3 char").isLength({ min:3 }),
+        check("email", "email is required").isEmail(),
+        check("password", "password is should be at least 3 char").isLength({ min: 3 })
+    ],
+    signup
+);
+
+
+router.post("/signin", [
+        check("email", "email is required").isEmail(),
+        check("password", "password is should be at least 3 char").isLength({ min: 1 })
+    ],
+    signin
+);
 
 
 router.get("/signout", signout);
