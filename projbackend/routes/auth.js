00bd642719705376138a38be93fd4ca696413108
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2021-07-28 20:15:29 +0530

    data validation

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 const { signout, signup } = require("../controllers/auth");
+// const { body, validationResult } = require('express-validator');
+const { check } = require('express-validator');
 
-router.post("/signup", signup);
+
+router.post("/signup", [
+    check("name", "Name should be at least 3 char").isLength({ min:3 }),
+    check("email", "email is required").isEmail(),
+    check("password", "password is should be at least 3 char").isLength({ min: 3 })
+],signup);
 
 
 router.get("/signout", signout);
